ee4842829ee7170591e46d851e96eda449e5d9e0
SadPencil <sadpencil@outlook.com>
2026-03-16 01:08:27 +0800

    Explicitly enable tests parallelization

--- ToyRa2MdLauncherCSharpTest/Properties/AssemblyInfo.cs
+++ ToyRa2MdLauncherCSharpTest/Properties/AssemblyInfo.cs
@@ -1,5 +1,8 @@
-﻿using System.Runtime.InteropServices;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("cd22e2d4-29a1-4959-a59b-cf180d52600a")]
+
+[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
